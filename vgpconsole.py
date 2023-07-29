c809bf27d10c161825bc909a29be841d0b2d5163
greenturtledev <sculetoncode@gmail.com>
2023-07-28 23:08:24 -0700

    added the ability to view and labeled next (week's) games

--- vgpconsole.py
+++ vgpconsole.py
@@ -9,6 +9,10 @@ driver = webdriver.Chrome();
 websiteURL = "https://store.epicgames.com/en-US/";
 driver.get(websiteURL);
 
-for index in range(1, 3):
+for index in range(1, 5):
     title = driver.find_element(By.XPATH, "//*[@id='dieselReactWrapper']/div/div[4]/main/div[2]/div/div/div/span[4]/div/div/section/div/div[" + str(index) + "]/div/div/a/div/div/div[3]/div/div");
-    print(title.text);
+    if index == 1:
+        print("This week's free games are: ");
+    if index == 3:
+        print("\n" + "Next week's free games are: ");
+    print(f'[{index}] {title.text}');
