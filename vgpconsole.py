3972bbee2f0f58cf941632d56c88adc9a5344371
greenturtledev <sculetoncode@gmail.com>
2023-07-30 20:09:57 -0700

    added Firefox browser support

--- vgpconsole.py
+++ vgpconsole.py
@@ -4,10 +4,15 @@ from selenium.webdriver.common.by import By
 
 directoryPath = r"/Documents/Progamming/Visual Studio/Drivers";
 os.environ["PATH"] += directoryPath;
-driver = webdriver.Chrome();
+browser = input("Select a browser, {Chrome or Firefox}: ");
+if (browser.lower() == "chrome"):
+    driver = webdriver.Chrome();
+else:
+    driver = webdriver.Firefox();
 
 websiteURL = "https://store.epicgames.com/en-US/";
 driver.get(websiteURL);
+driver.implicitly_wait(2);
 
 for index in range(1, 5):
     title = driver.find_element(By.XPATH, "//*[@id='dieselReactWrapper']/div/div[4]/main/div[2]/div/div/div/span[4]/div/div/section/div/div[" + str(index) + "]/div/div/a/div/div/div[3]/div/div");
@@ -16,3 +21,4 @@ for index in range(1, 5):
     if index == 3:
         print("\n" + "Next week's free games are: ");
     print(f'[{index}] {title.text}');
+driver.close();
