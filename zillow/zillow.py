a4f59bb5919c74259a603ca1411ae72e85949f0c
greenturtledev <sculetoncode@gmail.com>
2023-08-06 18:57:10 -0700

    removed chrome support, only firefox and ability to search

--- zillow/zillow.py
+++ zillow/zillow.py
@@ -2,3 +2,9 @@ from zpackage.web import Web
 
 tracker = Web();
 tracker.open_page();
+tracker.search();
+try:
+    tracker.first_time_pop_up_preferences()
+except:
+    print("First time preferences have been ignored. Rerouting...")
+# tracker.close_page();
